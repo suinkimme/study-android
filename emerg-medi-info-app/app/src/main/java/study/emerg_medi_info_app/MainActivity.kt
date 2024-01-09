d6d7fe79cf9fb065ec14c40d1efd421f6688ecf2
suinkimme <contact@suinkim.me>
2024-01-09 22:30:13 +0900

    SharedPreferences 적용 완료

--- emerg-medi-info-app/app/src/main/java/study/emerg_medi_info_app/MainActivity.kt
+++ emerg-medi-info-app/app/src/main/java/study/emerg_medi_info_app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import study.emerg_medi_info_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,9 +20,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getData() {
+    override fun onResume() {
+        super.onResume()
+        getDataUiUpdate()
+    }
+
+    private fun getDataUiUpdate() {
         with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE)) {
             binding.nameValueTextView.text = getString(NAME, "미정")
+            binding.birthdateValueTextView.text = getString(BIRTHDATE, "미정")
+            binding.bloodTypeValueTextView.text = getString(BLOOD_TYPE, "미정")
+            binding.emerContactNumberValueTextView.text = getString(CONTACT, "미정")
+
+            val other = getString(OTHER, "")
+            binding.otherTextView.isVisible = other.isNullOrEmpty().not()
+            binding.otherValueTextView.isVisible = other.isNullOrEmpty().not()
+            if (!other.isNullOrEmpty()) {
+                binding.otherValueTextView.text = getString(OTHER, "미정")
+            }
         }
     }
 }
