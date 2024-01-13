f8449cc3c7099fc9780c0cb09e4c3442f86a7877
suinkimme <contact@suinkim.me>
2024-01-14 01:08:02 +0900

    타이머 UI 구성 완료함

--- timer/app/src/main/java/com/study/timer/MainActivity.kt
+++ timer/app/src/main/java/com/study/timer/MainActivity.kt
@@ -2,10 +2,68 @@ package com.study.timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import com.study.timer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener {
+            start()
+            binding.startButton.isVisible = false
+            binding.stopButton.isVisible = false
+            binding.pauseButton.isVisible = true
+            binding.lapButton.isVisible = true
+        }
+
+        binding.stopButton.setOnClickListener {
+            showAlertDialog()
+        }
+
+        binding.pauseButton.setOnClickListener {
+            pause()
+            binding.startButton.isVisible = true
+            binding.stopButton.isVisible = true
+            binding.pauseButton.isVisible = false
+            binding.lapButton.isVisible = false
+        }
+
+        binding.lapButton.setOnClickListener {
+            lap()
+        }
+    }
+
+    private fun start() {
+
+    }
+
+    private fun stop() {
+        binding.startButton.isVisible = true
+        binding.stopButton.isVisible = true
+        binding.pauseButton.isVisible = false
+        binding.lapButton.isVisible = false
+    }
+
+    private fun pause() {
+
+    }
+
+    private fun lap() {
+
+    }
+
+    private fun showAlertDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("종료하시겠습니까?")
+            setPositiveButton("네") { _, _, ->
+                stop()
+            }
+            setNegativeButton("아니오", null)
+        }
     }
 }
