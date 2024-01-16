8132a0500f93216ee3513514252e53182992cb18
suinkimme <contact@suinkim.me>
2024-01-16 22:22:15 +0900

    뷰 바인딩 사용

--- todo/app/src/main/java/com/study/todo/MainActivity.kt
+++ todo/app/src/main/java/com/study/todo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.study.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.study.todo.databinding.ActivityAddBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
