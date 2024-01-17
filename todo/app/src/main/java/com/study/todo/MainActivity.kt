12ab0598a4c0f0aaae66b10c8c93c6d10357b317
suinkimme <contact@suinkim.me>
2024-01-17 23:01:16 +0900

    리사이클러뷰 구현 완료

--- todo/app/src/main/java/com/study/todo/MainActivity.kt
+++ todo/app/src/main/java/com/study/todo/MainActivity.kt
@@ -2,13 +2,34 @@ package com.study.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.study.todo.databinding.ActivityAddBinding
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.study.todo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityAddBinding
+class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var wordAdapter: WordAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAddBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val dummyList = mutableListOf(
+            Word("weather", "날씨", "명사"),
+            Word("honey", "꿀", "명사"),
+            Word("Run", "실행하다", "동사"),
+        )
+        wordAdapter = WordAdapter(dummyList, this)
+        binding.wordRecyclerView.apply {
+            adapter = wordAdapter
+            layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            val dividerItemDecoration = DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
+    }
+
+    override fun onClick(word: Word) {
+        Toast.makeText(this, "${word.text}가 클릭 됐습니다", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ todo/app/src/main/java/com/study/todo/Word.kt
@@ -0,0 +1,7 @@
+package com.study.todo
+
+data class Word(
+    val text: String,
+    val mean: String,
+    val type: String,
+)
