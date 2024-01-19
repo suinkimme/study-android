3ee2811003d11f244574b5665d0f7c8af8402d5a
suinkimme <contact@suinkim.me>
2024-01-19 23:19:05 +0900

    수정 기능 작성

--- todo/app/src/main/java/com/study/todo/AddActivity.kt
+++ todo/app/src/main/java/com/study/todo/AddActivity.kt
@@ -4,18 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.view.children
 import com.google.android.material.chip.Chip
 import com.study.todo.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddBinding
+    private var originWord: Word? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.addButton.setOnClickListener {
-            add()
+            if (originWord == null) add() else edit()
         }
 
         initView()
@@ -27,6 +29,14 @@ class AddActivity : AppCompatActivity() {
             types.forEach { text ->
                 addView(createChip(text))
             }
+        }
+
+        originWord = intent.getParcelableExtra("originWord")
+        originWord?.let {word ->
+            binding.textInputEditText.setText(word.text)
+            binding.meanInputEditText.setText(word.mean)
+            val selectedChip = binding.typeChipGroup.children.firstOrNull { (it as Chip).text === word.type } as? Chip
+            selectedChip?.isChecked = true
         }
     }
 
@@ -52,6 +62,23 @@ class AddActivity : AppCompatActivity() {
             val intent = Intent().putExtra("isUpdated", true)
             setResult(RESULT_OK, intent)
             finish()
+        }.start()
+    }
+
+    private fun edit() {
+        val text = binding.textInputEditText.text.toString()
+        val mean = binding.meanInputEditText.text.toString()
+        val type = findViewById<Chip>(binding.typeChipGroup.checkedChipId).text.toString()
+        val editWord = originWord?.copy(text = text, mean = mean, type = type)
+
+        Thread {
+            editWord?.let { word ->
+                AppDatabase.getInstance(this)?.wordDao()?.update(word)
+                val intent = Intent().putExtra("editWord", editWord)
+                setResult(RESULT_OK)
+                runOnUiThread { Toast.makeText(this, "수정을 완료했습니다", Toast.LENGTH_SHORT).show() }
+                finish()
+            }
         }.start()
     }
 }
--- todo/app/src/main/java/com/study/todo/MainActivity.kt
+++ todo/app/src/main/java/com/study/todo/MainActivity.kt
@@ -22,6 +22,21 @@ class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
             updateAddWord()
         }
     }
+    private val updateEditWordResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val editWord = result.data?.getParcelableExtra<Word>("editWord")
+        if (result.resultCode == RESULT_OK && editWord != null) {
+            updateEditWord(editWord)
+        }
+    }
+
+    private fun updateEditWord(word: Word) {
+        val index = wordAdapter.list.indexOfFirst { it.id == word.id }
+        wordAdapter.list[index] = word
+        runOnUiThread { wordAdapter.notifyDataSetChanged() }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -32,6 +47,14 @@ class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
             Intent(this, AddActivity::class.java).let {
                 updateAddWordResult.launch(it)
             }
+        }
+
+        binding.deleteImageView.setOnClickListener {
+            delete()
+        }
+
+        binding.editImageView.setOnClickListener {
+            edit()
         }
     }
 
@@ -51,11 +74,35 @@ class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
         }.start()
      }
 
+    private fun edit() {
+        if (selectedWord == null) return;
+
+        val intent = Intent(this, AddActivity::class.java).putExtra("originWord", selectedWord)
+        updateEditWordResult.launch(intent)
+    }
+
     private fun updateAddWord() {
         Thread {
             AppDatabase.getInstance(this)?.wordDao()?.getLatestWord()?.let { word ->
                 wordAdapter.list.add(0, word)
                 runOnUiThread { wordAdapter.notifyDataSetChanged() }
+            }
+        }.start()
+    }
+
+    private fun delete() {
+        if (selectedWord == null) return
+
+        Thread {
+            selectedWord?.let { word ->
+                AppDatabase.getInstance(this)?.wordDao()?.delete(word)
+                runOnUiThread {
+                    wordAdapter.list.remove(word)
+                    wordAdapter.notifyDataSetChanged()
+                    binding.textTextView.text = ""
+                    binding.meanTextView.text = ""
+                    Toast.makeText(this, "삭제가 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                }
             }
         }.start()
     }
