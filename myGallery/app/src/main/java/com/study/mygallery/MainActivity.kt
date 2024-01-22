4fa9c4c199b22de68b15ee82ad6f08cdad00562d
suinkimme <contact@suinkim.me>
2024-01-22 21:11:20 +0900

    권한 설정

--- myGallery/app/src/main/java/com/study/mygallery/MainActivity.kt
+++ myGallery/app/src/main/java/com/study/mygallery/MainActivity.kt
@@ -2,10 +2,31 @@ package com.study.mygallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.study.mygallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loadImageButton.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+    }
+
+    private fun loadImage() {
+
+    }
+
+    companion object {
+        const val REQUEST_READ_EXTERNAL_STORAGE = 100
     }
 }
