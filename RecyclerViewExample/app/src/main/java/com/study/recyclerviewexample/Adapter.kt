0730525d18e71a2aa294c013133d428f66992ff5
suinkimme <contact@suinkim.me>
2024-01-25 23:07:18 +0900

    예제 완성

--- RecyclerViewExample/app/src/main/java/com/study/recyclerviewexample/Adapter.kt
+++ RecyclerViewExample/app/src/main/java/com/study/recyclerviewexample/Adapter.kt
@@ -1,13 +1,15 @@
 package com.study.recyclerviewexample
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.study.recyclerviewexample.databinding.ItemBinding
 
-class Adapter(private val items: ArrayList<Item>): RecyclerView.Adapter<Adapter.ViewHolder>() {
+class Adapter(private val items: List<Item>): RecyclerView.Adapter<Adapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        Log.d("RecyclerView Review", "onCreateViewHolder")
         val inflater = parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val binding = ItemBinding.inflate(inflater, parent, false)
         return ViewHolder(binding)
@@ -18,6 +20,7 @@ class Adapter(private val items: ArrayList<Item>): RecyclerView.Adapter<Adapter.
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.i("RecyclerView Review", "onBindViewHolder")
         val item = items[position]
         holder.bind(item)
     }
--- RecyclerViewExample/app/src/main/java/com/study/recyclerviewexample/MainActivity.kt
+++ RecyclerViewExample/app/src/main/java/com/study/recyclerviewexample/MainActivity.kt
@@ -2,19 +2,88 @@ package com.study.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.study.recyclerviewexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val items = listOf(
+        Item("안녕하세요"),
+        Item("감사해요"),
+        Item("잘 있어요"),
+        Item("다시 만나요"),
+        Item("안녕하세요"),
+        Item("감사해요"),
+        Item("잘 있어요"),
+        Item("다시 만나요"),
+        Item("아침해가 뜨면"),
+        Item("매일 같은 사람들과"),
+        Item("또 다시"),
+        Item("새로운 하루 일을"),
+        Item("시작해"),
+        Item("안녕하세요"),
+        Item("감사해요"),
+        Item("잘 있어요"),
+        Item("다시 만나요"),
+        Item("안녕하세요"),
+        Item("감사해요"),
+        Item("잘 있어요"),
+        Item("다시 만나요"),
+        Item("아침해가 뜨면"),
+        Item("매일 같은 사람들과"),
+        Item("또 다시"),
+        Item("새로운 하루 일을"),
+        Item("시작해"),
+        Item("안녕하세요"),
+        Item("감사해요"),
+        Item("잘 있어요"),
+        Item("다시 만나요"),
+        Item("안녕하세요"),
+        Item("감사해요"),
+        Item("잘 있어요"),
+        Item("다시 만나요"),
+        Item("아침해가 뜨면"),
+        Item("매일 같은 사람들과"),
+        Item("또 다시"),
+        Item("새로운 하루 일을"),
+        Item("시작해"),
+        Item("안녕하세요"),
+        Item("감사해요"),
+        Item("잘 있어요"),
+        Item("다시 만나요"),
+        Item("안녕하세요"),
+        Item("감사해요"),
+        Item("잘 있어요"),
+        Item("다시 만나요"),
+        Item("아침해가 뜨면"),
+        Item("매일 같은 사람들과"),
+        Item("또 다시"),
+        Item("새로운 하루 일을"),
+        Item("시작해"),
+        Item("안녕하세요"),
+        Item("감사해요"),
+        Item("잘 있어요"),
+        Item("다시 만나요"),
+        Item("안녕하세요"),
+        Item("감사해요"),
+        Item("잘 있어요"),
+        Item("다시 만나요"),
+        Item("아침해가 뜨면"),
+        Item("매일 같은 사람들과"),
+        Item("또 다시"),
+        Item("새로운 하루 일을"),
+        Item("시작해"),
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val list = ArrayList<Item>()
-        list.add(Item("안녕하세요"))
-        list.add(Item("감사해요"))
-        list.add(Item("잘있어요"))
-        list.add(Item("다시 만나요"))
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerView.adapter = Adapter(items)
     }
 }
