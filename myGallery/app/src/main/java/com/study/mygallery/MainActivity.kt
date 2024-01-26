eb93ba5ad46ba2bee3a9d505aefde1e7996906ee
suinkimme <contact@suinkim.me>
2024-01-26 22:47:08 +0900

    권한요청 -> 교육용 팝업 포함

--- myGallery/app/src/main/java/com/study/mygallery/MainActivity.kt
+++ myGallery/app/src/main/java/com/study/mygallery/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.study.mygallery
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.study.mygallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,14 +23,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_MEDIA_IMAGES
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+            }
+            shouldShowRequestPermissionRationale(
+                android.Manifest.permission.READ_MEDIA_IMAGES
+            ) -> {
+                showPermissionInfoDialog()
+            }
+            else -> {
+                requestReadMediaImage()
+            }
+        }
+    }
+
+    private fun showPermissionInfoDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("이미지를 가져오기 위해서 외부 저장소 읽기 권한이 필요합니다.")
+            setNegativeButton("취소", null)
+            setPositiveButton("동의") { _, _ ->
+                requestReadMediaImage()
+            }
+        }.show()
+    }
+
+    private fun requestReadMediaImage() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES), REQUEST_READ_MEDIA_IMAGES)
     }
 
     private fun loadImage() {
-
+        Toast.makeText(this, "이미지를 가져올 예정", Toast.LENGTH_SHORT).show()
     }
 
     companion object {
-        const val REQUEST_READ_EXTERNAL_STORAGE = 100
+        const val REQUEST_READ_MEDIA_IMAGES = 100
     }
 }
