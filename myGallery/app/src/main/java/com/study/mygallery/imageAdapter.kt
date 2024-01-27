678488f9e7843290c238814b0062617ef04af7ac
Suin Kim <contact@suinkim.me>
2024-01-27 22:56:19 +0900

    이미지 뷰 레이아웃 추가

--- myGallery/app/src/main/java/com/study/mygallery/imageAdapter.kt
+++ myGallery/app/src/main/java/com/study/mygallery/imageAdapter.kt
@@ -9,21 +9,43 @@ import androidx.recyclerview.widget.RecyclerView
 class imageAdapter: ListAdapter<ImageItems, RecyclerView.ViewHolder>(
     object : DiffUtil.ItemCallback<ImageItems>() {
         override fun areItemsTheSame(oldItem: ImageItems, newItem: ImageItems): Boolean {
-            TODO("Not yet implemented")
+            return oldItem === newItem
         }
 
         override fun areContentsTheSame(oldItem: ImageItems, newItem: ImageItems): Boolean {
-            TODO("Not yet implemented")
+            return oldItem == newItem
         }
 
     }
 ) {
+
+    override fun getItemCount(): Int {
+        val originSize = currentList.size
+        return if (originSize == 0) 0 else originSize.inc()
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (itemCount.dec() == position) ITEM_LOAD_MORE else ITEM_IMAGE
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        return when (viewType) {
+            ITEM_IMAGE -> {
+
+            }
+            else -> {
+
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        const val ITEM_IMAGE = 0
+        const val ITEM_LOAD_MORE = 1
     }
 }
 
@@ -33,4 +55,8 @@ sealed class ImageItems {
     ): ImageItems()
 
     object LoadMore: ImageItems()
+}
+
+class ImageViewHolder(): RecyclerView.ViewHolder() {
+
 }
